4aa9164cb3f7f16a665a76407bc076bcaa54094e
edward <edward_gio@hotmail.com>
2025-01-17 20:45:57 -0500

    Core fundamentals (Class, constructor, inheritance, override)

---
+++ oop_fundamentals/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+var animal = new Animal("Animal name", 5);
+animal.MakeSound();
+
+var pitbull = new Dog("Pancho", 3, "Pitbull");
+pitbull.MakeSound();
+
+Console.ReadLine();
+
+// Mold
+public class Animal
+{
+    // Properties
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    // Constructor
+    public Animal(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    // Functions
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Makes a sound");
+    }
+}
+
+// Inheritance
+public class Dog : Animal
+{
+    public string Race { get; set; }
+
+    public Dog(string name, int age, string race) : base(name, age)
+    {
+        Race = race;
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("Barks");
+    }
+}
+
+
